33c21649e73f81870d70ca00e5b69278c07320db
lintzuhsiang <b02901069@ntu.edu.tw>
2017-03-25 00:17:14 +0800

    new

--- doctorbot/fb_doctor_chatbot/urls.py
+++ doctorbot/fb_doctor_chatbot/urls.py
@@ -3,5 +3,6 @@ from .views import hello_world
 from .views import Doctor
 
 urlpatterns = [
-	url(r'^bbf803c2a28d7bf5aa984866a317ca4f16f0dd4cda939b3fda/?$',Doctor.as_view())
+	url(r'^fb/?$',Doctor.as_view()),
+	url(r'^doctor/?$',hello_world),
 ]
