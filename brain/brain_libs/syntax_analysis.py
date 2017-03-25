9acb046d9b0c9b63e73382cdc3b999a6e9c4b352
behappycc <behappycc@hotmail.com>
2017-03-26 01:11:09 +0800

    add zero_padding

--- brain/brain_libs/syntax_analysis.py
+++ brain/brain_libs/syntax_analysis.py
@@ -37,16 +37,33 @@ class SyntaxAnalysis(object):
         return corpus_list
 
     def one_hot_encode(self, corpus_list, sentence_words):
+        one_hot_words = np.array([])
         for s_word in sentence_words:
-            one_hot_list = np.zeros(len(corpus_list) + 1)
+            one_hot_word = np.zeros(len(corpus_list) + 1)
             if s_word not in corpus_list:
-                one_hot_list[-1] = 1
-                print('word: {} one-hot: {}'.format(s_word, one_hot_list))
+                one_hot_word[-1] = 1
+                print('word: {} one-hot: {}'.format(s_word, one_hot_word))
             else:
                 for index, word in enumerate(corpus_list):
                     if word == s_word:
-                        one_hot_list[index] = 1
-                        print('word: {} one-hot: {}'.format(s_word, one_hot_list))
+                        one_hot_word[index] = 1
+                        print('word: {} one-hot: {}'.format(s_word, one_hot_word))
+            one_hot_words = np.append(one_hot_words, one_hot_word)
+        print (one_hot_words)
+        print (one_hot_words.shape)
+        return one_hot_words
+
+    def zero_padding(self, one_hot_words, time_steps, input_size):
+        print('xxx')
+        padding_num = int(time_steps - (len(one_hot_words) / input_size))
+        print(padding_num)
+        one_hot_word = np.zeros(input_size)
+        for i in range(padding_num):
+            one_hot_words = np.append(one_hot_words, one_hot_word)
+        print (one_hot_words.shape)
+        print (one_hot_words)
+        print (one_hot_words.reshape(30, 4))
+        return one_hot_words
 
 
 def main():
@@ -55,7 +72,8 @@ def main():
     words = ['妳好', '妳好', '妳好', '妳', '好']
     sentence_words = ['妳好', '妳', '好', '哈']
     corpus_list = sa.generate_corpus(words)
-    sa.one_hot_encode(corpus_list, sentence_words)
+    one_hot_words = sa.one_hot_encode(corpus_list, sentence_words)
+    sa.zero_padding(one_hot_words, 30, 4)
 
 
 if __name__ == '__main__':
