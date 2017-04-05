1166ac1d41a207f8ba1d7b63983f471709c40eba
zuxfoucault <zuxfoucault@gmail.com>
2017-04-05 12:20:18 +0800

    add data_resource/week_dict.txt

--- brain/brain_libs/data_resource/create_dictionary.py
+++ brain/brain_libs/data_resource/create_dictionary.py
@@ -31,6 +31,9 @@ def doctor_list_generator(division_file, doctor_list):
                     if len(item) <= 3 and doctor_list.count(item) == 0:
                         doctor_list.append(item)
 
+def week_list_generator(week_file, week_list):
+    for row in week_file:
+        week_list.append(row[0])
 
 def main():
 
@@ -38,15 +41,20 @@ def main():
     division_file = csv.reader(div_rf)
     dis_rf = open("disease.csv", "r")
     disease_file = csv.reader(dis_rf)
+    week_rf = open("week.csv", "r")
+    week_file = csv.reader(week_rf)
     disease_list = []
     division_list = []
     doctor_list = []
+    week_list = []
     disease_list_generator(disease_file, disease_list, division_list)
     doctor_list_generator(division_file, doctor_list)
+    week_list_generator(week_file, week_list)
 
     doc_f = open("doctor_dict.txt", "w")
     dis_f = open("disease_dict.txt", "w")
     div_f = open("division_dict.txt", "w")
+    week_f = open("week_dict.txt", "w")
     o_f = open("other_dict.txt", "w")
     print('請問', file=o_f)
     for word in disease_list:
@@ -55,12 +63,16 @@ def main():
         print(word, file=div_f)
     for word in doctor_list:
         print(word, file=doc_f)
+    for word in week_list:
+        print(word, file=week_f)
 
     div_rf.close()
     dis_rf.close()
+    week_rf.close()
     doc_f.close()
     dis_f.close()
     div_f.close()
+    week_f.close()
     o_f.close()
 
 if __name__ == '__main__':
