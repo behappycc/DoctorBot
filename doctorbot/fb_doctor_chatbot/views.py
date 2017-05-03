caadca30368414e0b52273ebe85d02926db5846d
lintzuhsiang <b02901069@ntu.edu.tw>
2017-05-03 09:56:35 +0000

    fb api

--- doctorbot/fb_doctor_chatbot/views.py
+++ doctorbot/fb_doctor_chatbot/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+import json, requests, random, re
+from pprint import pprint
 
 
-TOKEN = 'AG1BsKUxZB8BAIwX45aWjehyGbfqWgu7DVVorr4qhe77V9mrtETGurNiUH45z7nPAhM2A68yNDZCSvb9dZB8UyTB9p3DUz6hpnVjiQlgKVNrDmJa6n3PXDxc9NKYhIZC4f7LdZA7qjYFdEXsD3FtzCxaQKlC83VFIvUqko7V6gZDZD'
+
+#@ensure_csrf_cookie
+verify_token = '00000000'
+
+TOKEN='EAAG1BsKUxZB8BAKB6qwvZCXbsHSGNKHWMCVuMSS92MI7owsqYMEwAREXksTEq2x3nDO5kJGefmIfJLS7RZBFWegXlhMBR6ChiTNva3wD6ASxNTVomQ0h79ZBAUQgsamtLovGGWbW4MsyFZCY82vP3zcngJgOU8oedNCJvNYcptwZDZD'
 
 # Create your views here.
 class Doctor(generic.View):
-	#@method_decorator(csrf_exempt)
+	@method_decorator(csrf_exempt)
 	def dispatch(self,request,*args,**kwargs):
 		return generic.View.dispatch(self,request,*args,**kwargs)
 
@@ -21,7 +30,7 @@ class Doctor(generic.View):
 		return HttpResponse()
 
 	def get(self,request,*args,**kwargs):
-		if self.request.GET== TOKEN :
+		if self.request.GET['hub.verify_token']== verify_token :
 			return HttpResponse(self.request.GET['hub.challenge'])
 		else:
 			return HttpResponse('Error,invalid token')
@@ -29,31 +38,15 @@ class Doctor(generic.View):
 
 
 
-jokes = {
-         'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
-                    """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
-         'fat':    ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""",
-                    """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """],
-         'dumb':   ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""",
-                    """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""] 
-         }
 # This function should be outside the BotsView class
 def post_facebook_message(fbid, recevied_message):           
 
  # Remove all punctuations, lower case the text and split it based on space
     tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
-    joke_text = ''
-    for token in tokens:
-        if token in jokes:
-            joke_text = random.choice(jokes[token])
-            break
-    if not joke_text:
-        joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!"
-
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=<page-access-token>' 
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%TOKEN 
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
 def hello_world(request):
-	return HttpResponse("Hello world!!!!!!!!")
+	return HttpResponse("Hello world!")
